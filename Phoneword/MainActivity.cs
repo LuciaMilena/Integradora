8a7800965df9240c8cc50a1fd9657b327e3c9d70
facundoaguel <faguel@eest1chivilcoy.edu.ar>
2019-11-26 18:38:40 -0300

    me falta hacer el primer uso con u if para crear el arvicho y hacer comentarios

--- Phoneword/MainActivity.cs
+++ Phoneword/MainActivity.cs
@@ -5,14 +5,15 @@ using Core;
 using System.Collections.Generic;
 using Android.Content;
 using Android.Preferences;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace Phoneword
 {
     [Activity(Label = "Phone Word", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        static readonly List<string> phoneNumbers = new List<string>();
-
+        static List<string> phoneNumbers = new List<string>();
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -26,63 +27,86 @@ namespace Phoneword
             if (primerUso)
             {
                 //Do things if it's NOT the first run of the app... 
-                
+
                 //codigo de lectura
 
                 ISharedPreferencesEditor editor = prefs.Edit();
                 editor.PutBoolean("PrimerUso", false);
                 // editor.Commit();    // applies changes synchronously on older APIs
                 editor.Apply();
-            } 
-            else 
-            {                  
+            }
+            else
+            {
                 //Do things if it IS the first run of the app...
-            } 
+            }
 
-                // Set our view from the "main" layout resource
-                SetContentView(Resource.Layout.Main);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
 
-                // Get our UI controls from the loaded layout
-                EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
-                Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
-                Button translationHistoryButton = FindViewById<Button>(Resource.Id.TranslationHistoryButton);
-                TextView translatedPhoneWord = FindViewById<TextView>(Resource.Id.TranslatedPhoneWord);
+            // Get our UI controls from the loaded layout
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            Button translationHistoryButton = FindViewById<Button>(Resource.Id.TranslationHistoryButton);
+            TextView translatedPhoneWord = FindViewById<TextView>(Resource.Id.TranslatedPhoneWord);
 
-                // Add code to translate number
-                string translatedNumber = string.Empty;
+            // Add code to translate number
+            string translatedNumber = string.Empty;
 
-                translateButton.Click += (sender, e) =>
+            translateButton.Click += (sender, e) =>
+            {
+                    // Translate user’s alphanumeric phone number to numeric
+                    translatedNumber = PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                if (string.IsNullOrWhiteSpace(translatedNumber))
                 {
-                // Translate user’s alphanumeric phone number to numeric
-                translatedNumber = PhonewordTranslator.ToNumber(phoneNumberText.Text);
-                    if (string.IsNullOrWhiteSpace(translatedNumber))
-                    {
-                        translatedPhoneWord.Text = string.Empty;
-                    }
-                    else
-                    {
-                        translatedPhoneWord.Text = translatedNumber;
-                        phoneNumbers.Add(translatedNumber);
-                        translationHistoryButton.Enabled = true; // aca se activa el boton del translate history
-                                                                 //aca es donde se agrega el "Guardar"
+                    translatedPhoneWord.Text = string.Empty;
                 }
-                };
-
-                translationHistoryButton.Click += (sender, e) =>
+                else
                 {
-                    var intent = new Intent(this, typeof(TranslationHistoryActivity));
-                    intent.PutStringArrayListExtra("phone_numbers", phoneNumbers);
-                    StartActivity(intent);
-                };
+                    translatedPhoneWord.Text = translatedNumber;
+                    phoneNumbers.Add(translatedNumber);
+                    translationHistoryButton.Enabled = true; // aca se activa el boton del translate history
+                    Guardar();                               //aca es donde se agrega el "Guardar"
+                }
+            };
+
+            translationHistoryButton.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(TranslationHistoryActivity));
+                intent.PutStringArrayListExtra("phone_numbers", phoneNumbers);
+                StartActivity(intent);
+            };
+        }
+
+        public void Cargar()
+        {
+            using (StreamReader lector = new StreamReader("Contactos.json"))
+            {
+                string ContactosJson = lector.ReadToEnd();
+                phoneNumbers = JsonConvert.DeserializeObject<List<string>>(ContactosJson);
             }
         }
-        //public async Task SaveCountAsync(int count)
-        //{
-        //    var backingFile = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "count.txt");
-        //    using (var writer = File.CreateText(backingFile))
-        //    {
-        //        await writer.WriteLineAsync(count.ToString());
-        //    }
-        //}
-       
+
+        public void Guardar()
+        {
+            {
+                using (StreamWriter escritor = new System.IO.StreamWriter("Contactos.json"))
+                {
+                    string ContactosJson = JsonConvert.SerializeObject(phoneNumbers);
+                    escritor.Write(ContactosJson);
+                }
+            }
+        }
     }
+
+    
+    //public async Task SaveCountAsync(int count)
+    //{
+    //    var backingFile = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "count.txt");
+    //    using (var writer = File.CreateText(backingFile))
+    //    {
+    //        await writer.WriteLineAsync(count.ToString());
+    //    }
+    //}
+
+
+}
