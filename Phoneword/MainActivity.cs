82b36636f278f9bc46e57a4f31b3ad4238b8b2c9
info-1 <info-1@INFO3-PC8>
2019-11-29 08:26:08 -0300

    ya esta

--- Phoneword/MainActivity.cs
+++ Phoneword/MainActivity.cs
@@ -13,86 +13,89 @@ namespace Phoneword
     [Activity(Label = "Phone Word", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        static List<string> phoneNumbers = new List<string>();
+        static List<string> phoneNumbers = new List<string>();//
 
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override void OnCreate(Bundle savedInstanceState)//
         {
-            base.OnCreate(savedInstanceState);
+            base.OnCreate(savedInstanceState);//
 
             // https://stackoverflow.com/questions/26668509/how-do-i-use-sharedpreferences-in-xamarin-android
 
-            ISharedPreferences prefs = PreferenceManager.GetDefaultSharedPreferences(this);
-            bool primerUso = prefs.GetBoolean("PrimerUso", true);
+            /*ISharedPreferences prefs = PreferenceManager.GetDefaultSharedPreferences(this);//
+            bool primerUso = prefs.GetBoolean("PrimerUso", true);//
 
-            if (primerUso)
+            if (primerUso)//
             {
                 //Do things if it's NOT the first run of the app... 
 
                 //codigo de lectura
 
-                ISharedPreferencesEditor editor = prefs.Edit();
-                editor.PutBoolean("PrimerUso", false);
+                ISharedPreferencesEditor editor = prefs.Edit();//
+                editor.PutBoolean("PrimerUso", false);//
                 // editor.Commit();    // applies changes synchronously on older APIs
-                editor.Apply();
+                editor.Apply();//
             }
-            else
+            else//
             {
                 //Do things if it IS the first run of the app...
-            }
+            };*/
 
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
+            SetContentView(Resource.Layout.Main);//
 
             // Get our UI controls from the loaded layout
-            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
-            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
-            Button translationHistoryButton = FindViewById<Button>(Resource.Id.TranslationHistoryButton);
-            TextView translatedPhoneWord = FindViewById<TextView>(Resource.Id.TranslatedPhoneWord);
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);//
+            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);//
+            Button translationHistoryButton = FindViewById<Button>(Resource.Id.TranslationHistoryButton);//
+            TextView translatedPhoneWord = FindViewById<TextView>(Resource.Id.TranslatedPhoneWord);//
 
             // Add code to translate number
-            string translatedNumber = string.Empty;
+            string translatedNumber = string.Empty;//
 
-            translateButton.Click += (sender, e) =>
+            translateButton.Click += (sender, e) =>//
             {
                     // Translate user’s alphanumeric phone number to numeric
-                    translatedNumber = PhonewordTranslator.ToNumber(phoneNumberText.Text);
-                if (string.IsNullOrWhiteSpace(translatedNumber))
+                    translatedNumber = PhonewordTranslator.ToNumber(phoneNumberText.Text);//
+                if (string.IsNullOrWhiteSpace(translatedNumber))//
                 {
-                    translatedPhoneWord.Text = string.Empty;
+                    translatedPhoneWord.Text = string.Empty;//
                 }
                 else
                 {
-                    translatedPhoneWord.Text = translatedNumber;
-                    phoneNumbers.Add(translatedNumber);
+                    translatedPhoneWord.Text = translatedNumber;//
+                    phoneNumbers.Add(translatedNumber);//
                     translationHistoryButton.Enabled = true; // aca se activa el boton del translate history
                     Guardar();                               //aca es donde se agrega el "Guardar"
                 }
             };
 
-            translationHistoryButton.Click += (sender, e) =>
+            translationHistoryButton.Click += (sender, e) =>//
             {
-                var intent = new Intent(this, typeof(TranslationHistoryActivity));
-                intent.PutStringArrayListExtra("phone_numbers", phoneNumbers);
-                StartActivity(intent);
+                var intent = new Intent(this, typeof(TranslationHistoryActivity));//
+                intent.PutStringArrayListExtra("phone_numbers", phoneNumbers);//
+                StartActivity(intent);//
             };
         }
 
-        public void Cargar()
+        public void Cargar()//
         {
-            using (StreamReader lector = new StreamReader("Contactos.json"))
+            if (File.Exists("Contactos.json"))
             {
-                string ContactosJson = lector.ReadToEnd();
-                phoneNumbers = JsonConvert.DeserializeObject<List<string>>(ContactosJson);
+                using (StreamReader lector = new StreamReader("Contactos.json"))//
+                {
+                    string ContactosJson = lector.ReadToEnd();//
+                    phoneNumbers = JsonConvert.DeserializeObject<List<string>>(ContactosJson);//
+                }
             }
         }
 
-        public void Guardar()
+        public void Guardar()//
         {
             {
-                using (StreamWriter escritor = new System.IO.StreamWriter("Contactos.json"))
+                using (StreamWriter escritor = new System.IO.StreamWriter("Contactos.json"))//
                 {
-                    string ContactosJson = JsonConvert.SerializeObject(phoneNumbers);
-                    escritor.Write(ContactosJson);
+                    string ContactosJson = JsonConvert.SerializeObject(phoneNumbers);//
+                    escritor.Write(ContactosJson);//
                 }
             }
         }
